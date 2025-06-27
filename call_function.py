6d4ade72cc8d6c4d40194dcdc3602f4ca344ed7b
jfaverill <jfaverill@gmail.com>
2025-06-27 15:32:04 -0500

    Incorporate agent function calling

--- call_function.py
+++ call_function.py
@@ -1,8 +1,8 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_get_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python import schema_get_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
 
 available_functions = types.Tool(
     function_declarations = [
@@ -12,3 +12,38 @@ available_functions = types.Tool(
         schema_write_file
     ]
 )
+
+def call_function(function_call_part, verbose = False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    functions = {"get_files_info": get_files_info,
+                 "get_file_content": get_file_content,
+                 "write_file": write_file,
+                 "run_python_file": run_python_file}
+    if function_name not in functions:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    function = functions[function_name]
+    function_args = function_call_part.args
+    function_args["working_directory"] = "./calculator"
+    function_result = function(**function_args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
-from call_function import available_functions
+from call_function import available_functions, call_function
 from prompts import system_prompt
 
 def main():
@@ -46,7 +46,11 @@ def main():
         return response.text
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if not function_call_result.parts[0].function_response.response:
+            raise Exception("Fatal: No function response")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 
 if __name__ == "__main__":
     main()
